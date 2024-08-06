fb663eceac084ad2945136c0724e34abc8adfbad
ZingerLittleBee <6970999@gmail.com>
2024-08-06 19:25:52 +0800

    insert text and image

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -49,14 +49,77 @@ impl DB {
     }
 
     pub async fn insert_text(&self, input: TextModel) -> anyhow::Result<()> {
+        self.client
+            .query(
+                "
+        CREATE text CONTENT {
+	        data: $data,
+	        vector: $vector
+        }",
+            )
+            .bind(input)
+            .await?;
         Ok(())
     }
 
     pub async fn insert_image(&self, input: ImageModel) -> anyhow::Result<()> {
+        self.client
+            .query(
+                "
+        CREATE image CONTENT {
+	        url: $url,
+	        prompt: $prompt,
+            vector: $vector
+        }",
+            )
+            .bind(input)
+            .await?;
         Ok(())
     }
 
     pub async fn insert_item(&self, input: ItemModel) -> anyhow::Result<()> {
         Ok(())
+    }
+}
+
+mod test {
+    use dotenvy::dotenv;
+
+    async fn setup() -> crate::db::DB {
+        dotenv().ok();
+        crate::db::DB::new().await
+    }
+
+    #[tokio::test]
+    async fn test_insert_text() {
+        let db = setup().await;
+        let text = crate::model::TextModel {
+            data: "Hello, World!".to_string(),
+            vector: vec![0.0, 1.0, 2.0],
+        };
+        db.insert_text(text).await.unwrap();
+        let res = db
+            .client
+            .query("SELECT * FROM text where data = 'Hello, World!'")
+            .await
+            .unwrap();
+        println!("res: {:?}", res);
+    }
+
+    #[tokio::test]
+    async fn test_insert_image() {
+        let db = setup().await;
+        let image = crate::model::ImageModel {
+            url: "https://example.com".to_string(),
+            prompt: "What is in this picture?".to_string(),
+            vector: vec![0.0, 1.0, 2.0],
+        };
+        db.insert_image(image).await.unwrap();
+        let res = db
+            .client
+            .query("SELECT * FROM image where url = 'https://example.com'")
+            .await
+            .unwrap();
+        println!("res: {:?}", res);
     }
 }
--- src/model/mod.rs
+++ src/model/mod.rs
@@ -1,21 +1,27 @@
+use serde::{Deserialize, Serialize};
+
 pub mod input;
 
+#[derive(Serialize, Deserialize)]
 pub struct ImageModel {
     pub url: String,
     pub prompt: String,
     pub vector: Vec<f32>,
 }
 
+#[derive(Serialize, Deserialize)]
 pub struct ItemModel {
     pub text: Vec<TextModel>,
     pub image: Vec<ImageModel>,
 }
 
+#[derive(Serialize, Deserialize)]
 pub struct TextModel {
     pub data: String,
     pub vector: Vec<f32>,
 }
 
+#[derive(Serialize, Deserialize)]
 pub enum DataModel {
     Text(TextModel),
     Image(ImageModel),
