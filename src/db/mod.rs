2868205fd019382e19746e06b870d8276c97c272
ZingerLittleBee <6970999@gmail.com>
2024-08-06 14:04:48 +0800

    init table

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -1,6 +1,6 @@
 mod sql;
 
-use crate::db::sql::CREATE_ITEM_TABLE;
+use crate::db::sql::CREATE_TABLE;
 use surrealdb::{
     engine::remote::ws::{Client, Ws},
     opt::auth::Root,
@@ -27,31 +27,12 @@ impl DB {
         })
         .await?;
         db.use_ns("dam").use_db("search").await?;
-        DB::init_content_table(&db).await?;
+        DB::init_table(&db).await?;
         Ok(db)
     }
 
-    async fn init_content_table(db: &Surreal<Client>) -> anyhow::Result<()> {
-        db.query(CREATE_ITEM_TABLE).await?;
-
-        db.query(r#"
-        -- 插入多条文本记录
-CREATE text SET data = 'Text 1', vector = [0.1, 0.2, 0.3];
-CREATE text SET data = 'Text 2', vector = [0.4, 0.5, 0.6];
-
--- 插入多条图像记录
-CREATE image SET url = 'https://example.com/image1.jpg', vector = [0.7, 0.8, 0.9], prompt = 'Image 1';
-CREATE image SET url = 'https://example.com/image2.jpg', vector = [1.0, 1.1, 1.2], prompt = 'Image 2';
-
--- 在 "item" 表中插入一条记录，并建立与多条 "text" 和 "image" 记录的关联关系
-LET $texts = (SELECT id FROM text WHERE data IN ['Text 1', 'Text 2']);
-LET $images = (SELECT id FROM image WHERE url IN ['https://example.com/image1.jpg', 'https://example.com/image2.jpg']);
-
-CREATE item SET
-  text = (RELATE $texts),
-  image = (RELATE $images);
-        "#).await?;
-
+    async fn init_table(db: &Surreal<Client>) -> anyhow::Result<()> {
+        db.query(CREATE_TABLE).await?;
         Ok(())
     }
 }
--- src/db/sql.rs
+++ src/db/sql.rs
@@ -1,28 +1,33 @@
-pub const CREATE_ITEM_TABLE: &str = r#"
+pub const CREATE_TABLE: &str = r#"
 -- 创建 "item" 表
 DEFINE TABLE IF NOT EXISTS item;
 -- 定义 "item" 表的字段
-DEFINE FIELD IF NOT EXISTS id ON TABLE item TYPE string DEFAULT rand::uuid::v7();
 DEFINE FIELD IF NOT EXISTS text ON TABLE item TYPE array<record<text>>;
 DEFINE FIELD IF NOT EXISTS image ON TABLE item TYPE array<record<image>>;
 
 -- 创建 "text" 表
 DEFINE TABLE IF NOT EXISTS text;
 -- 定义 "text" 表的字段
-DEFINE FIELD IF NOT EXISTS id ON TABLE text TYPE string DEFAULT rand::uuid::v7();
 DEFINE FIELD IF NOT EXISTS data ON TABLE text TYPE string;
 DEFINE FIELD IF NOT EXISTS vector ON TABLE text TYPE array;
 
 -- 创建 "image" 表
 DEFINE TABLE IF NOT EXISTS image;
 -- 定义 "image" 表的字段
-DEFINE FIELD IF NOT EXISTS id ON TABLE image TYPE string DEFAULT rand::uuid::v7();
 DEFINE FIELD IF NOT EXISTS url ON TABLE image TYPE string;
 DEFINE FIELD IF NOT EXISTS vector ON TABLE image TYPE array;
 DEFINE FIELD IF NOT EXISTS prompt ON TABLE image TYPE string;
 
--- 为每个表的 "id" 字段创建唯一索引
-DEFINE INDEX IF NOT EXISTS idx_item_id ON item COLUMNS id UNIQUE;
-DEFINE INDEX IF NOT EXISTS idx_text_id ON text COLUMNS id UNIQUE;
-DEFINE INDEX IF NOT EXISTS idx_image_id ON image COLUMNS id UNIQUE;
-    "#;
+-- 创建 "frame" 表
+DEFINE TABLE IF NOT EXISTS frame;
+-- 定义 "frame" 表的字段
+DEFINE FIELD IF NOT EXISTS point ON TABLE frame TYPE datetime;
+DEFINE FIELD IF NOT EXISTS text ON TABLE frame TYPE record<text>;
+DEFINE FIELD IF NOT EXISTS image ON TABLE frame TYPE record<image>;
+
+-- 创建 "video" 表
+DEFINE TABLE IF NOT EXISTS video;
+-- 定义 "video" 表的字段
+DEFINE FIELD IF NOT EXISTS url ON TABLE video TYPE string;
+DEFINE FIELD IF NOT EXISTS frame ON TABLE video TYPE array<record<frame>>;
+"#;
