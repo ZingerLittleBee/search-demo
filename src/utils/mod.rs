5f08e6d8c0993138070b4dff9842f5f8040452c8
ZingerLittleBee <6970999@gmail.com>
2024-08-14 19:31:55 +0800

    mock data

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,1 +1,21 @@
+use regex::Regex;
+use tracing::info;
+
 pub mod image;
+
+pub fn replace_single_quotes(input: &str) -> String {
+    let re = Regex::new(r"'").unwrap();
+    let res = re.replace_all(input, "\'").to_string();
+
+    info!("origin: {input}, replaced: {res}");
+
+    res
+}
+
+#[cfg(test)]
+mod test {
+    #[test]
+    fn test_replace_single_quotes() {
+        assert_eq!(super::replace_single_quotes("I'm"), "I\'m");
+    }
+}
